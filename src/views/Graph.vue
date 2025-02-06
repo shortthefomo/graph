000daf1c71a8ca1197279f6a0e8f1615d2228569
Lathan Britz <jucallme@gmail.com>
2025-02-06 17:43:22 -0400

    lets do it

--- src/views/Graph.vue
+++ src/views/Graph.vue
@@ -105,7 +105,7 @@ export default {
                 { label: '10', value: 10 },
                 { label: '50', value: 50 },
                 { label: '100', value: 100 },
-                { label: '105', value: 150 },
+                { label: '150', value: 150 },
                 { label: '200', value: 200 },
                 { label: '250', value: 250 },
                 { label: '300', value: 300 },
