c802f868bc5a422b2ef0c994133d764041ac4944
Lathan Britz <jucallme@gmail.com>
2025-03-19 18:31:26 -0400

    dev

--- src/views/Graph.vue
+++ src/views/Graph.vue
@@ -124,16 +124,13 @@ export default {
             range: 10,
             ranges: [
                 { label: '10', value: 10 },
+                { label: '25', value: 25 },
                 { label: '50', value: 50 },
+                { label: '75', value: 75 },
                 { label: '100', value: 100 },
                 { label: '150', value: 150 },
                 { label: '200', value: 200 },
                 { label: '250', value: 250 },
-                { label: '300', value: 300 },
-                { label: '350', value: 350 },
-                { label: '400', value: 400 },
-                { label: '450', value: 450 },
-                { label: '500', value: 500 },
             ],
             interaction: true,
             networks: [
